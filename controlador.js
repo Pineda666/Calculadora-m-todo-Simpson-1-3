307da913f82fee5af11c61ad3293a1503c62ccec
Carlos Pineda <cpinedaurquiza@gmail.com>
2022-12-26 13:51:27 -0500

    version 2.0 sin css jaja

--- controlador.js
+++ controlador.js
@@ -32,6 +32,32 @@ function ingresarValoresFuncion() {
 }
 */
 
+function mostrarFuncionNormal() {
+    let valoresfx = [];
+    let valorDeH = parseFloat(calcularH());
+    let limiteInferior = parseFloat(document.getElementById('a').value);
+    let numeroIntervalos = parseInt(document.getElementById('n').value);
+    let valorX = 0;
+
+    for (let i = 0; i < numeroIntervalos + 1; i++) {
+        let fx = document.getElementById('fx').value;
+        fx = fx.split('');
+        valorX = limiteInferior + (i * valorDeH);
+
+        for (let j = 0; j < fx.length; j++) {
+            switch (fx[j]) {
+                case 'x':
+                    fx[j] = valorX;
+                    break;
+            }
+        }
+        fx = fx.join('');
+
+        valoresfx.push(fx);
+    }
+    return valoresfx;
+}
+
 
 function reescribirFuncion() {
     let fx = document.getElementById('fx').value;
@@ -39,23 +65,21 @@ function reescribirFuncion() {
 
     for (let i = 0; i < fx.length; i++) {
         switch (fx[i]) {
-            case '^':
-                fx[i] = 'Math.pow(' + fx[i - 1] + ',' + fx[i + 1] + ')';
-                fx[i - 1] = '';
-                fx[i + 1] = '';
+            case '^'://reconocer exponente
+                fx[i] = 'Math.pow';
                 break;
             case 's':
-                if (fx[i + 1] == 'e' && fx[i + 2] == 'n') {
+                if (fx[i + 1] == 'e' && fx[i + 2] == 'n') {//reconocer seno
                     fx[i] = 'Math.sin';
                     fx[i + 1] = '';
                     fx[i + 2] = '';
-                } else if (fx[i + 1] == 'q') {
+                } else if (fx[i + 1] == 'q') {//reconocer raíz cuadrada
                     fx[i] = 'Math.sqrt';
                     fx[i + 1] = '';
                 } else {
                     break;
                 }
-            case 'c':
+            case 'c'://reconocer coseno
                 if (fx[i + 1] == 'o' && fx[i + 2] == 's') {
                     fx[i] = 'Math.cos';
                     fx[i + 1] = '';
@@ -64,11 +88,11 @@ function reescribirFuncion() {
                     break;
                 }
             case 'a':
-                if (fx[i + 1] == 'b' && fx[i + 2] == 's') {
+                if (fx[i + 1] == 'b' && fx[i + 2] == 's') {//reconocer valor absoluto
                     fx[i] = 'Math.abs';
                     fx[i + 1] = '';
                     fx[i + 2] = '';
-                } else if (fx[i + 1] == 'c' && fx[i + 2] == 'o' && fx[i + 3] == 's') {
+                } else if (fx[i + 1] == 'c' && fx[i + 2] == 'o' && fx[i + 3] == 's') {//reconocer arco coseno
                     fx[i] = 'Math.acos';
                     fx[i + 1] = '';
                     fx[i + 2] = '';
@@ -76,7 +100,7 @@ function reescribirFuncion() {
                 } else {
                     break;
                 }
-            case 't':
+            case 't'://reconocer tangente
                 if (fx[i + 1] == 'a' && fx[i + 2] == 'n') {
                     fx[i] = 'Math.tan';
                     fx[i + 1] = '';
@@ -84,7 +108,7 @@ function reescribirFuncion() {
                 } else {
                     break;
                 }
-            case 'l':
+            case 'l'://reconocer logaritmo
                 if (fx[i + 1] == 'o' && fx[i + 2] == 'g') {
                     fx[i] = 'Math.log';
                     fx[i + 1] = '';
@@ -103,15 +127,15 @@ function reescribirFuncion() {
 
 
 function calcularFx() {
-    valoresFx = [];
-    var valorDeH = parseFloat(calcularH());
-    var limiteInferior = parseFloat(document.getElementById('a').value);
-    var numeroIntervalos = parseInt(document.getElementById('n').value);
-    var valorX = 0;
+    let valoresFx = [];
+    let valorDeH = parseFloat(calcularH());
+    let limiteInferior = parseFloat(document.getElementById('a').value);
+    let numeroIntervalos = parseInt(document.getElementById('n').value);
+    let valorX;
     let resultados;
 
 
-    for (let i = 0; i < numeroIntervalos + 1; i++) {
+    for (let i = 0; i <= numeroIntervalos; i++) {
         let fx = reescribirFuncion();
         fx = fx.split('');
         valorX = limiteInferior + (i * valorDeH);
@@ -131,12 +155,12 @@ function calcularFx() {
     return valoresFx;
 }
 
-function calcular() {
-    var valoresFx = calcularFx();
-    var resultado;
-    var h = calcularH();
-    var sumatoriaPar = 0;
-    var sumatorioImpar = 0;
+function imprimirResultadoFinal() {
+    let valoresFx = calcularFx();
+    let resultado;
+    let h = calcularH();
+    let sumatoriaPar = 0;
+    let sumatorioImpar = 0;
 
     for (i = 1; i < valoresFx.length - 1; i++) {
         if (i % 2 != 0) {
@@ -147,8 +171,50 @@ function calcular() {
     }
 
     resultado = (h / 3) * (parseFloat(valoresFx[0]) + sumatoriaPar + sumatorioImpar + parseFloat(valoresFx[valoresFx.length - 1]));
-    resultadoRedondeado = resultado.toFixed(4);
+    let resultadoRedondeado = resultado.toFixed(4);
 
-    alert("La integral por el método de Simpson 1/3 compuesto es:\n" + resultadoRedondeado);
+    let resultadoFinal = document.getElementById('resultadoFinal');
+    resultadoFinal.innerHTML = '';
+    let datoParrafo = document.createElement('p');
+    datoParrafo.innerText = "La integral por el método de Simpson 1/3 compuesto es:\n" + resultadoRedondeado;
+    resultadoFinal.appendChild(datoParrafo);
+}
 
+function imprimirFx() {
+    let funcionNormal = mostrarFuncionNormal();
+    let valoresFx = calcularFx();
+    let resultado = document.getElementById('resultadoFx');
+    resultado.innerHTML = '';
+
+    for (let i = 0; i < valoresFx.length; i++) {
+        let datoParrafo = document.createElement('p');
+        datoParrafo.innerText = 'f(' + i + ') = ' + funcionNormal[i] + ' = ' + valoresFx[i];
+
+        resultado.appendChild(datoParrafo);
+    }
+}
+
+
+function imprimirXi(){
+    let resultado = document.getElementById('resultadoXi');
+    resultado.innerHTML = '';
+    let h = calcularH();
+    let numeroIntervalos = parseInt(document.getElementById('n').value);
+    let limiteInferior = parseFloat(document.getElementById('a').value);
+    let X;
+
+    for(let i=0; i <= numeroIntervalos; i++){
+        X = limiteInferior + (i*h);
+
+        let datoParrafo = document.createElement('p');
+        datoParrafo.innerText = 'X(' + i + ') = '+limiteInferior +' + ('+i+'*'+h+')= ' + X;
+
+        resultado.appendChild(datoParrafo);
+    }
+}
+
+function calcular(){
+    imprimirResultadoFinal();
+    imprimirFx();
+    imprimirXi();
 }
